22efe74a142451777fc21001331b15ebd2348fd2
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-05-19 19:11:18 +0300

    looper0 for printing loop
    printing specified numbers using for loop

---
+++ CPP/looper0.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+  for(int i=1;i<=10;i++){
+    // (initialization,condition,increment)
+    // inorder to break the loop, condition must become false. Increment will help do that.
+    cout<<i<<endl;
+    // prints the value of i which starting from 1 upto 10 in a newline of their own.
+  }
+  return 0;
+}
