e4ff3f68f60aa4a923d3196ea5864f4960297755
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-05-26 22:17:08 +0300

    looper1 A square * print using while loop

---
+++ CPP/Patterns/squarestar/looper1.cpp
@@ -0,0 +1,20 @@
+using while
+#include <iostream>
+using namespace std;
+int main(){
+
+int i=5,j = 5;
+
+while(j>0){
+  while(i>0){
+    cout<<"  *";
+    i--;
+    
+    }
+  i=5;
+  cout<<endl;
+  j--;
+  
+}
+  return 0;
+}
