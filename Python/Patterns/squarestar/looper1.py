72d65d7c13f1c67acd13e0f8dceb133b9ee666b0
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-05-26 22:29:20 +0300

    looper1 while printing square asterisk

---
+++ Python/Patterns/squarestar/looper1.py
@@ -0,0 +1,10 @@
+j=5
+
+while(j>0):
+  i=5
+  while(i>0):
+    print("  *",end="") # to continue printing sideways(to avoid printing vertically as python usually do)
+    i-=1
+  print() # to print on a new line
+  
+  j-=1
