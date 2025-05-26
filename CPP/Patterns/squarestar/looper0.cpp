02a4b2be443070a7a2ce3dc326bf95d60ce7386e
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-05-26 22:16:08 +0300

    looper0 A square * print

---
+++ CPP/Patterns/squarestar/looper0.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+
+for (int j=0;j<=4;j++){
+ for(int i=0;i<=4;i++){
+    cout<<"  *";   
+ }
+ cout<<endl;
+} 
+  return 0;
+}
+
+//A square 5 by  5 asterix print
