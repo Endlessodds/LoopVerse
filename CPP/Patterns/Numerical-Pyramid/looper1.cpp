4c80e33f056a808d8f410472a4e12a7ee0460410
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-06-03 23:15:42 +0300

    Create looper1

---
+++ CPP/Patterns/Numerical-Pyramid/looper1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int i,j;
+j=1;
+while(j<=5){
+  i =1;
+while(i<=j){
+  cout<<i<<"  ";
+  i++;
+  
+  
+}
+  cout<<endl;
+  j++;
+  
+}
+
+  return 0;
+}
+
